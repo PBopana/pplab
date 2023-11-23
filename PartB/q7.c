e4e0eada15f0918f30d029e2f1be59829e4cd34d
PBopana <94296539+PBopana@users.noreply.github.com>
2023-11-23 17:12:22 +0530

    Update MPI - Broadcast.c

--- PartB/q7.c
+++ PartB/q7.c
@@ -6,14 +6,14 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     int number;
+    // Root process is the one that broadcasts, so only it can initialise data
     if (world_rank == 0) {
-    // The root process sets the number to be broadcasted
         number = 42;
         printf("Process 0 broadcasting number %d to all processes\n", number);
     }
-    // All processes call MPI_Bcast, which broadcasts the value from root to everyone
+    // Broadcast the number from the root process to all processes
     MPI_Bcast(&number, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // Now all processes have the number
+    // Print the received data at each process
     printf("Process %d received number %d\n", world_rank, number);
     MPI_Finalize();
     return 0;
