ad750fd7dfa1e0e52ca31b581cd64e549d9f6bac
PBopana <94296539+PBopana@users.noreply.github.com>
2023-11-23 19:20:48 +0530

    Update MPI - Allreduce

--- PartB/q12.c
+++ PartB/q12.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
-int main() {
+int main()
+{
     MPI_Init(NULL, NULL);
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -16,8 +17,16 @@ int main() {
     // Allreduce to find the product of all values
     MPI_Allreduce(&data, &prod, 1, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
     // Each process prints the results
-    printf("Process %d - Max: %d, Min: %d, Sum: %d, Product: %d\n",
-                world_rank, max_val, min_val, sum, prod);
+    printf("Process %d - Max: %d, Min: %d, Sum: %d, Product: %d\n", world_rank, max_val, min_val, sum, prod);
     MPI_Finalize();
     return 0;
 }
+
+/* MPI_Allreduce -
+    &data: Address of the data to be reduced.
+    &max_val: Address where the result of the reduction
+    1: The number of elements in the send buffer.
+    MPI_INT: Data type of the elements in the send buffer (in this case, integers).
+    MPI_MAX: Specifies the operation to be applied during the reduction
+    MPI_COMM_WORLD: Communicator, a group of processes that can communicate with each other.
+*/
