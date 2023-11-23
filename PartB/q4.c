0a8186e0b2d95491feb9d9d393a6a0dd3df136c5
PBopana <94296539+PBopana@users.noreply.github.com>
2023-11-23 18:55:37 +0530

    Update MPI - Deadlock Avoidance2

--- PartB/q4.c
+++ PartB/q4.c
@@ -1,25 +1,35 @@
+// Deadlock Avoidance Non blocking calls
 #include <stdio.h>
 #include <mpi.h>
-int main() {
+int main()
+{
     int world_rank,world_size;
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    // MPI_Request for handling non-blocking calls
     MPI_Request request;
     MPI_Status status;
-    if (world_size < 2) {
+    // Check if there are at least 2 processes for communication
+    if (world_size < 2)
+    {
         printf("World size must be greater than 1\n");
         MPI_Finalize();
         return 0;
     }
     int number = 77;
-    if (world_rank == 0) {
+    if (world_rank == 0)
+    {
+        // Non-blocking send from process 0 to 1, wait for its completion and then do non-blocking receive from process 1
         MPI_Isend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
         MPI_Irecv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
         printf("Process 0 received number %d from process 1\n", number);
-    } else if (world_rank == 1) {
+    }
+    else if (world_rank == 1)
+    {
+        // Non-blocking recieve from process 0, wait for its completion and then do non-blocking send from process 1 to 0
         MPI_Irecv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
         MPI_Wait(&request, &status);
         MPI_Isend(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
