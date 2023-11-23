fb37a22b37c5232a8bb11f277143252dedba5b4c
PBopana <94296539+PBopana@users.noreply.github.com>
2023-11-23 17:08:53 +0530

    Update MPI- Gather

--- PartB/q8.c
+++ PartB/q8.c
@@ -5,19 +5,20 @@ int main() {
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    // Each process will send its rank
+    // send_data - Data that each process sends to root
     int send_data = world_rank;
-    // The root process will collect all ranks
+    // Array for root process to gather other process data
     int recv_data[world_size];
     // Gather data from all processes to root (rank 0)
-    MPI_Gather(&send_data, 1, MPI_INT, 
-                recv_data, 1, MPI_INT, 
-                0, MPI_COMM_WORLD);
+    MPI_Gather(&send_data, 1, MPI_INT, recv_data, 1, MPI_INT, 0, MPI_COMM_WORLD);
     // Root process prints the gathered data
-    if (world_rank == 0) {
+    if (world_rank == 0) 
+    {
         printf("Gathered data at root: ");
-        for (int i = 0; i < world_size; i++) 
+        for (int i = 0; i < world_size; i++)
+        {
             printf("%d \t", recv_data[i]);
+        }
     }
     MPI_Finalize();
     return 0;
